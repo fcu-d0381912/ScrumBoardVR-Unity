90c63d2c18f967835a270e553d483a4d3c99ea53
fcu-d0342358 <d0342358@fcu.edu.tw>
2017-11-17 17:17:45 +0800

    fcu-d0342358 專案捲軸三維位移轉二維位移 fcu-d0381908 專案捲軸使用手把拖曳 fcu-d0381912 登入介面各項UI調整+專案UI微調

--- project_test01/Assets/ScrollProject.cs
+++ project_test01/Assets/ScrollProject.cs
@@ -14,6 +14,14 @@ public class ScrollProject : MonoBehaviour {
 	float tempX;
 	float tempY;
 
+
+	private float controllerX;
+	private float controllerY;
+	private Vector3 controllerPosition = new Vector3(0,0,0);
+	private bool gripPressDown = false;
+	private int moveCount = 0;
+
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
 	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
 	private SteamVR_TrackedObject trackedObj;
@@ -23,11 +31,11 @@ public class ScrollProject : MonoBehaviour {
 	void Start () {
 		trackedObj = GetComponent<SteamVR_TrackedObject>();
 		centerLeft = movePanel.GetComponent<RectTransform> ().position;
-		centerRight = movePanel.GetComponent<RectTransform> ().offsetMax;
+		centerRight = movePanel.GetComponent<RectTransform> ().offsetMin;
 		newCenterLeft = movePanel.GetComponent<RectTransform> ().offsetMin;
 		newCenterRight = movePanel.GetComponent<RectTransform> ().offsetMax;
-		tempX=buttonArray [4].GetComponent<RectTransform> ().anchoredPosition.x;
-		tempY=buttonArray [4].GetComponent<RectTransform> ().anchoredPosition.y;
+		//tempX=buttonArray [4].GetComponent<RectTransform> ().anchoredPosition.x;
+		//tempY=buttonArray [4].GetComponent<RectTransform> ().anchoredPosition.y;
 
 	
 	}
@@ -37,9 +45,15 @@ public class ScrollProject : MonoBehaviour {
 
 
 		for(i=0;i<buttonArray.Length;i++){
-			if((buttonArray[i].transform.position.x-centerLeft.x)>5){
+			Debug.Log (centerLeft.x + "," + buttonArray [0].transform.position.x);
+			if((buttonArray[i].transform.position.x - centerLeft.x)>6){
 				Debug.Log (buttonArray [i].transform.position + " " + " " + centerLeft.x);
-				buttonArray [i].GetComponent<RectTransform> ().position = new Vector3 (-3, centerLeft.y,2);
+				newCenterLeft = movePanel.GetComponent<RectTransform> ().offsetMin;
+				buttonArray [i].GetComponent<RectTransform> ().anchoredPosition = new Vector2(((-1)* newCenterLeft.x)-50,buttonArray [i].GetComponent<RectTransform> ().anchoredPosition.y);
+			}
+			else if((buttonArray[i].transform.position.x + centerLeft.x)<-6){
+			//	Debug.Log (buttonArray [i].transform.position + " " + " " + centerLeft.x);
+				buttonArray [i].GetComponent<RectTransform> ().anchoredPosition = new Vector2(((-1)* newCenterRight.x)+50,buttonArray [i].GetComponent<RectTransform> ().anchoredPosition.y);
 			}
 		}
 
@@ -68,7 +82,52 @@ public class ScrollProject : MonoBehaviour {
 		else if(controller.GetHairTriggerUp()){
 			newCenterLeft = movePanel.GetComponent<RectTransform> ().offsetMin;
 			newCenterRight = movePanel.GetComponent<RectTransform> ().offsetMax;
+
 		}
+
+		else if(controller.GetPressDown(gripButton)){
+			gripPressDown = true;
+			controllerPosition.x = transform.position.x;
+		}
+
+
+		else if(controller.GetPressUp(gripButton)){
+			gripPressDown = false;
+
+
+		}
+		else if(gripPressDown){
+			
+			if(controllerPosition.x - transform.position.x < - 0.15){
+				movePanel= movePanel.gameObject;
+
+				movePanel.GetComponent<RectTransform> ().offsetMin =new Vector2(newCenterLeft.x + 100, 0);
+				movePanel.GetComponent<RectTransform> ().offsetMax =new Vector2(newCenterRight.x + 100, 0);
+				newCenterLeft = movePanel.GetComponent<RectTransform> ().offsetMin;
+				newCenterRight = movePanel.GetComponent<RectTransform> ().offsetMax;
+				moveCount = moveCount + 1;
+				Debug.Log ("move" + moveCount);
+				controllerPosition.x = transform.position.x;
+			}else if( controllerPosition.x - transform.position.x > 0.15){
+				movePanel= movePanel.gameObject;
+
+				movePanel.GetComponent<RectTransform> ().offsetMin =new Vector2(newCenterLeft.x - 100, 0);
+				movePanel.GetComponent<RectTransform> ().offsetMax =new Vector2(newCenterRight.x - 100, 0);
+				newCenterLeft = movePanel.GetComponent<RectTransform> ().offsetMin;
+				newCenterRight = movePanel.GetComponent<RectTransform> ().offsetMax;
+				moveCount = moveCount + 1;
+				Debug.Log ("move" + moveCount);
+				controllerPosition.x = transform.position.x;
+
+			}
+				
+		}
+
+		/*else if(gripPressDown){
+			
+		}
+		*/
+
 
 		//Debug.Log (this.transform.position.x);
 	}
